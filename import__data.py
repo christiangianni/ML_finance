95585b79e6d7e70f4e6e7b4a4b16eec3388ac452
christian <christian.gianni@gmail.com>
2022-05-16 23:37:51 +0200

    start how to download from yahoo we have 2 tools https://www.analyticsvidhya.com/blog/2021/06/download-financial-dataset-using-yahoo-finance-in-python-a-complete-guide/

--- import__data.py
+++ import__data.py
@@ -3,7 +3,8 @@
 import pandas as pd
 import yfinance as yf
 from yahoofinancials import YahooFinancials
-import matplotlib
+import matplotlib.pyplot as plt
+plt.show()
 
 
 def import_from_yfinance(name):
@@ -16,8 +17,14 @@ def import_from_yfinance(name):
     aapl_df.head()
     print(aapl_df.head())
     ticker = yf.Ticker('AAPL')
-    aapl_df = ticker.history(period="5y")
+    aapl_df = ticker.history(period="1mo")  #valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
     aapl_df['Close'].plot(title="APPLE's stock price")
+    print(ticker.recommendations)
+    print(ticker.dividends)
+    print(ticker.earnings)
+
+
+
     print('0')
 
 
@@ -32,8 +39,21 @@ def import_from_yahoo_financials(name):
     aapl_df.head()
     print(aapl_df.head())
 
+
+def test_plot():
+    plt.show()
+    time = [0, 1, 2, 3]
+    position = [0, 100, 200, 300]
+
+    plt.plot(time, position)
+    plt.xlabel('Time (hr)')
+    plt.ylabel('Position (km)')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     import_from_yfinance('AAPL')
-    import_from_yahoo_financials('AAPL')
+    #import_from_yahoo_financials('AAPL')
+    #test_plot()
+    plt.show()
     print('1')
